37e8c2db0c38dd9fe19716a35e83c788c8c93c80
ganxiaohua <543123001@qq.com>
2025-03-22 15:53:09 +0800

    update waitTime

--- Runtime/Timer/WaitTime.cs
+++ Runtime/Timer/WaitTime.cs
@@ -8,7 +8,20 @@ namespace GameFrame.Runtime.Timer
     {
         private int versions;
 
-        public async UniTask WaitSec(float sec, CancellationToken cancellationToken = default)
+        public async UniTask WaitSecs(float sec, CancellationToken cancellationToken = default)
+        {
+            try
+            {
+                await Wait(sec, cancellationToken);
+            }
+            catch (Exception e)
+            {
+                if (e is not OperationCanceledException)
+                    Debugger.LogError(e);
+            }
+        }
+
+        private async UniTask Wait(float sec, CancellationToken cancellationToken = default)
         {
             int ver = ++versions;
             await UniTask.Delay((int) (sec * 1000), false, PlayerLoopTiming.Update, cancellationToken);
@@ -17,8 +30,22 @@ namespace GameFrame.Runtime.Timer
                 throw new OperationCanceledException(cancellationToken);
             }
         }
-        
-        public async UniTask WaitFrame( CancellationToken cancellationToken = default)
+
+
+        public async UniTask WaitFrame(CancellationToken cancellationToken = default)
+        {
+            try
+            {
+                await Wait(cancellationToken);
+            }
+            catch (Exception e)
+            {
+                if (e is not OperationCanceledException)
+                    Debugger.LogError(e);
+            }
+        }
+
+        private async UniTask Wait(CancellationToken cancellationToken = default)
         {
             int ver = ++versions;
             await UniTask.Yield();
