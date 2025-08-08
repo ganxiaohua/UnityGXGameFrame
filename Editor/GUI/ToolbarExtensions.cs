c91e13af88f5fecb5c13700527c67989097dd608
ganxiaohua <543123001@qq.com>
2025-08-08 21:56:54 +0800

    update  toolbarextensions

--- Editor/GUI/ToolbarExtensions.cs
+++ Editor/GUI/ToolbarExtensions.cs
@@ -10,7 +10,8 @@ namespace GameFrame.Editor
     [InitializeOnLoad]
     public static class ToolbarExtensions
     {
-        private static List<(GUIContent, Action)> customButtons = new List<(GUIContent, Action)>();
+        private static List<(GUIContent, Action)> customRightButtons = new List<(GUIContent, Action)>();
+        private static List<(GUIContent, Action)> customLeftButtons = new List<(GUIContent, Action)>();
 
         static ToolbarExtensions()
         {
@@ -26,26 +27,51 @@ namespace GameFrame.Editor
                     var zone = root.Q("ToolbarZoneRightAlign");
                     var parent = new VisualElement()
                     {
-                            style =
-                            {
-                                    flexGrow = 1,
-                                    flexDirection = FlexDirection.Row,
-                            }
+                        style =
+                        {
+                            flexGrow = 1,
+                            flexDirection = FlexDirection.Row,
+                        }
                     };
                     var container = new IMGUIContainer();
-                    container.onGUIHandler += OnToolbar;
+                    container.onGUIHandler += OnRightToolbar;
+                    parent.Add(container);
+                    zone.Add(parent);
+                    zone = root.Q("ToolbarZoneLeftAlign");
+                    parent = new VisualElement()
+                    {
+                        style =
+                        {
+                            flexGrow = 1,
+                            flexDirection = FlexDirection.Row,
+                        }
+                    };
+                    container = new IMGUIContainer();
+                    container.onGUIHandler += OnLeftToolbar;
                     parent.Add(container);
                     zone.Add(parent);
                 }
             };
         }
 
-        private static void OnToolbar()
+        private static void OnRightToolbar()
         {
             GUILayout.BeginHorizontal();
             if (GUILayout.Button("审查器"))
                 DialogueGraphWindow.OpenDialogueGraphWindow();
-            foreach (var (content, action) in customButtons)
+            foreach (var (content, action) in customRightButtons)
+            {
+                if (GUILayout.Button(content))
+                    action.Invoke();
+            }
+
+            GUILayout.EndHorizontal();
+        }
+
+        private static void OnLeftToolbar()
+        {
+            GUILayout.BeginHorizontal();
+            foreach (var (content, action) in customLeftButtons)
             {
                 if (GUILayout.Button(content))
                     action.Invoke();
@@ -55,14 +81,24 @@ namespace GameFrame.Editor
         }
 
 
-        public static void RegisterButton(string name, Action action)
+        public static void RegisterRightButton(string name, Action action)
         {
-            RegisterButton(name, string.Empty, action);
+            RegisterRightButton(name, string.Empty, action);
         }
 
-        public static void RegisterButton(string name, string icon, Action action)
+        public static void RegisterRightButton(string name, string icon, Action action)
         {
-            customButtons.Add((new GUIContent(name, EditorGUIUtility.FindTexture(icon)), action));
+            customRightButtons.Add((new GUIContent(name, EditorGUIUtility.FindTexture(icon)), action));
+        }
+
+        public static void RegisterLeftButton(string name, Action action)
+        {
+            RegisterLeftButton(name, string.Empty, action);
+        }
+
+        public static void RegisterLeftButton(string name, string icon, Action action)
+        {
+            customLeftButtons.Add((new GUIContent(name, EditorGUIUtility.FindTexture(icon)), action));
         }
     }
 }
