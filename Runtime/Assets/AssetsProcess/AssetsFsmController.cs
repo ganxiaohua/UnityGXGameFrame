5bc9c82a19303724e4f697f7e7aa196634c4e522
ganxiaohua <543123001@qq.com>
2025-08-12 16:44:05 +0800

    fixed ui bug

--- Runtime/Assets/AssetsProcess/AssetsFsmController.cs
+++ Runtime/Assets/AssetsProcess/AssetsFsmController.cs
@@ -5,13 +5,13 @@ namespace GameFrame.Runtime
     public class AssetsFsmController : FsmTaskController
     {
         private EDefaultBuildPipeline pipeline = EDefaultBuildPipeline.ScriptableBuildPipeline;
-        
+
         public override void OnInitialize()
         {
             base.OnInitialize();
             YooAssets.Initialize();
             var defPackage = YooConst.PackageSettings[0];
-            SetData("packageName",defPackage.name);
+            SetData("packageName", defPackage.name);
             SetData("playMode", defPackage.playMode);
             SetData("pipeline", pipeline);
             AddState<PackageInitState>();
