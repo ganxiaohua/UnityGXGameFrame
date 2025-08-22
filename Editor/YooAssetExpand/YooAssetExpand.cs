ed544127206bab8412fd7bde3f01af95e1352742
ganxiaohua <543123001@qq.com>
2025-08-22 08:12:37 +0800

    add yooassetExpand

--- Editor/YooAssetExpand/YooAssetExpand.cs
+++ Editor/YooAssetExpand/YooAssetExpand.cs
@@ -1,4 +1,6 @@
-﻿namespace YooAsset.Editor
+﻿using System.IO;
+
+namespace YooAsset.Editor
 {
     [DisplayName("定位地址: 文件夹_文件夹_文件名")]
     public class AddressByFolderFile : IAddressRule
@@ -6,7 +8,19 @@
         string IAddressRule.GetAssetAddress(AddressRuleData data)
         {
             string path = data.AssetPath.Replace(data.CollectPath, "");
-            string name = path.Substring(1, path.LastIndexOf('.')-1);
+            string name = path.Substring(1, path.LastIndexOf('.') - 1);
+            return name;
+        }
+    }
+
+
+    [DisplayName("定位地址: 文件名加后缀")]
+    public class AddressByName : IAddressRule
+    {
+        string IAddressRule.GetAssetAddress(AddressRuleData data)
+        {
+            string path = data.AssetPath.Replace(data.CollectPath, "");
+            string name = Path.GetFileName(path);
             return name;
         }
     }
