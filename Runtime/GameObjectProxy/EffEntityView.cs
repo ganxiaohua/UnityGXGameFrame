125f77fd49a1cfb169e3caa0f5d4444394ef0dd4
ganxiaohua <543123001@qq.com>
2025-08-23 11:01:16 +0800

    Update EffEntityView.cs

--- Runtime/GameObjectProxy/EffEntityView.cs
+++ Runtime/GameObjectProxy/EffEntityView.cs
@@ -5,23 +5,20 @@ namespace GameFrame.Runtime
     public class EffEntityView : GameObjectProxy
     {
         public EffEntity BindEntity { get; private set; }
-
+        private ViewEffBindEnitiy viewEffBindEnitiy;
         public override void Initialize(object initData)
         {
             base.Initialize(initData);
             AutoLayers = false;
             BindEntity = (EffEntity) initData;
+            viewEffBindEnitiy = gameObject.AddComponent<ViewEffBindEnitiy>();
+            viewEffBindEnitiy.Entity = BindEntity;
         }
         
-        protected override void OnAfterBind(GameObject go)
+        public override void Dispose()
         {
-            var bind = go.AddComponent<ViewEffBindEnitiy>();
-            bind.Entity = BindEntity;
-        }
-        
-        protected override void OnBeforeUnbind()
-        {
-            Object.Destroy(BindingTarget.GetComponent<ViewEffBindEnitiy>());
+            Object.Destroy(viewEffBindEnitiy);
+            base.Dispose();
         }
 
         public virtual void TickActive(float delatTime, float realElapseSeconds)
