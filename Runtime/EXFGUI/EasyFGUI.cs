ed11a8143e9ad8b5e139d4fa0b82d5ae98862c07
ganxiaohua <543123001@qq.com>
2025-10-29 16:09:01 +0800

    update easyFgui

--- Runtime/EXFGUI/EasyFGUI.cs
+++ Runtime/EXFGUI/EasyFGUI.cs
@@ -5,9 +5,10 @@ namespace UnityGXGameFrame.Runtime
 {
     public static class EasyFGUI
     {
+        //由于FGUI Y向下是增加,所以左上角是pos
         public static Rect Rect2Ins(this GObject gObject)
         {
-            var offset = new Vector2(-gObject.pivotX * gObject.width, (1 - gObject.pivotY) * gObject.height);
+            var offset = new Vector2(-gObject.pivotX * gObject.width, gObject.pivotY * gObject.height);
             return gObject.TransformRect(new Rect(offset.x, offset.y, gObject.width, gObject.height), null);
         }
     }
