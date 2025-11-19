2dc48cf05d7ff78e905cec67e4be33539dc74cc2
ganxiaohua <543123001@qq.com>
2025-11-19 15:07:26 +0800

    add  copyPathContextMenu

---
+++ Editor/Tool/CopyPathContextMenu.cs
@@ -0,0 +1,21 @@
+﻿using UnityEditor;
+using UnityEngine;
+
+namespace GameFrame.Editor
+{
+    public class CopyPathContextMenu : UnityEditor.Editor
+    {
+        [MenuItem("Assets/CopyAssetPath", false, 0)]
+        static void CopyFullPathFromHierarchy()
+        {
+            if (Selection.activeGameObject != null)
+            {
+                // 获取资源的相对路径
+                string assetPath = AssetDatabase.GetAssetPath(Selection.activeGameObject);
+                assetPath = assetPath.Substring(0, assetPath.LastIndexOf('.'));
+                // 将路径复制到剪贴板
+                EditorGUIUtility.systemCopyBuffer = assetPath;
+            }
+        }
+    }
+}
