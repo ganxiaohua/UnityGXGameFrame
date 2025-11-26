3cd56335db0326db5ef90c3ab916ab749c0581f9
ganxiaohua <543123001@qq.com>
2025-11-26 17:26:48 +0800

    update arrayExSimilar

---
+++ Editor/Tool/EnitiyGraph/ArrayExSimilar.cs
@@ -0,0 +1,77 @@
+﻿using System.Collections.Generic;
+using GameFrame.Runtime;
+
+namespace GameFrame.Editor
+{
+    public struct ArrayExSimilarData
+    {
+        public int Count;
+        public int State;
+    }
+
+    public class ArrayExSimilar
+    {
+        private ArrayEx<int> list;
+
+        // private int beforeIndex = 0;
+        // private List<int> similarIndex = new List<int>(128);
+        private List<ArrayExSimilarData> returnData = new List<ArrayExSimilarData>();
+        public string Name;
+
+        public ArrayExSimilar(int max, string name)
+        {
+            list = new ArrayEx<int>(max);
+            Name = name;
+        }
+
+        public void Add(int index, int value, int headFrame)
+        {
+            list[index] = value;
+            CalculateData(headFrame);
+        }
+
+        //TODO: 等待优化,先能用再说
+        private List<ArrayExSimilarData> CalculateData(int headFrame)
+        {
+            returnData.Clear();
+            int beforeState = list[headFrame];
+            int count = list.Count;
+            int n = 0;
+            for (int i = 0; i < list.Count; i++)
+            {
+                n++;
+                int index = (headFrame + i) % count;
+                var curState = list[index];
+                if (curState == 0)
+                {
+                    if (i != 0)
+                    {
+                        var curData = new ArrayExSimilarData();
+                        curData.Count = n;
+                        curData.State = beforeState;
+                        returnData.Add(curData);
+                    }
+
+                    break;
+                }
+
+                if (curState != beforeState || i == list.Count - 1)
+                {
+                    var curData = new ArrayExSimilarData();
+                    curData.Count = n;
+                    curData.State = beforeState;
+                    beforeState = curState;
+                    n = 0;
+                    returnData.Add(curData);
+                }
+            }
+
+            return returnData;
+        }
+
+        public List<ArrayExSimilarData> GetData()
+        {
+            return returnData;
+        }
+    }
+}
--- Editor/Tool/EnitiyGraph/CapabilityView.cs
+++ Editor/Tool/EnitiyGraph/CapabilityView.cs
@@ -1,20 +1,32 @@
 ﻿using System.Collections.Generic;
 using GameFrame.Runtime;
-using Sirenix.OdinInspector.Editor;
 using UnityEditor;
 using UnityEngine;
+using OdinEditorWindow = Sirenix.OdinInspector.Editor.OdinEditorWindow;
 
 namespace GameFrame.Editor
 {
     public class CapabilityView : OdinEditorWindow
     {
-        private static CapabilityView sWindow;
+        private enum CapabilityType
+        {
+            NotAction = 1,
+            Action,
+            Lock
+        }
 
+        private static CapabilityView sWindow;
         private EffEntity effEntity;
         private ECCWorld eccWorld;
-
         private List<CapabilityBase> capabilityBaseUpdateMode = new List<CapabilityBase>();
         private List<CapabilityBase> capabilityBaseFixUpdateMode = new List<CapabilityBase>();
+        private ArrayExSimilar[] updateMode;
+        private ArrayExSimilar[] fixUpdateMode;
+        private int tailFrame = 0;
+        private int headFrame = 0;
+        private int FrameSize = 500;
+        private Vector2 scrollPosition = Vector2.zero;
+        private Color[] stateColor = new Color[] {Color.white, Color.gray, Color.cyan, Color.yellow};
 
         public static void Init(EffEntity effEntity)
         {
@@ -31,12 +43,14 @@ namespace GameFrame.Editor
             capabilityBaseUpdateMode.Clear();
             capabilityBaseFixUpdateMode.Clear();
             eccWorld.GetCapability(effEntity, capabilityBaseUpdateMode, capabilityBaseFixUpdateMode);
+            updateMode = new ArrayExSimilar[capabilityBaseUpdateMode.Count];
+            fixUpdateMode = new ArrayExSimilar[capabilityBaseFixUpdateMode.Count];
         }
 
         protected override void OnBeginDrawEditors()
         {
             base.OnBeginDrawEditors();
-            if(effEntity == null)
+            if (effEntity == null)
                 return;
             if (effEntity.State == IEntity.EntityState.IsClear)
             {
@@ -44,23 +58,64 @@ namespace GameFrame.Editor
             }
 
             EditorGUILayout.LabelField("蓝色表示激活，白色表示未激活，黄色表示被锁住了");
+            if (EditorApplication.isPlaying && !EditorApplication.isPaused)
+            {
+                SetData(capabilityBaseUpdateMode, updateMode);
+                SetData(capabilityBaseFixUpdateMode, fixUpdateMode);
+                tailFrame++;
+                if (tailFrame >= FrameSize)
+                {
+                    headFrame = (tailFrame + 1) % FrameSize;
+                }
+            }
+
+            scrollPosition = EditorGUILayout.BeginScrollView(scrollPosition);
             EditorGUILayout.LabelField("___________________________update");
-            Show(capabilityBaseUpdateMode);
+            Draw(updateMode);
+            EditorGUILayout.Space(2 * updateMode.Length);
             EditorGUILayout.LabelField("___________________________fixedUpdate");
-            Show(capabilityBaseFixUpdateMode);
+            Draw(fixUpdateMode);
+            EditorGUILayout.EndScrollView();
         }
 
-        private void Show(List<CapabilityBase> baseMode)
+        private void SetData(List<CapabilityBase> baseMode, ArrayExSimilar[] capabilityTypes)
+        {
+            int count = baseMode.Count;
+            for (int i = 0; i < count; i++)
+            {
+                var capability = baseMode[i];
+                capabilityTypes[i] ??= new ArrayExSimilar(FrameSize, capability.GetType().Name);
+                bool islock = false;
+                if (capability.TagList != null)
+                    islock = eccWorld.IsBindCapability(effEntity, capability.TagList);
+                capabilityTypes[i].Add(tailFrame % FrameSize, (int) (islock ? CapabilityType.Lock : (capability.IsActive ? CapabilityType.Action : CapabilityType.NotAction)), headFrame);
+            }
+        }
+
+        private void Draw(ArrayExSimilar[] capabilityTypes)
         {
             var oldColor = GUI.contentColor;
-            foreach (var capability in baseMode)
+            for (int j = 0; j < capabilityTypes.Length; j++)
             {
+                var exSimilar = capabilityTypes[j];
+                var datas = exSimilar.GetData();
                 EditorGUILayout.BeginHorizontal();
-                GUI.color = capability.IsActive ? Color.cyan : Color.white;
-                if (capability.TagList != null)
-                    GUI.color = eccWorld.IsBindCapability(effEntity, capability.TagList) ? Color.yellow : GUI.color;
-                // EditorGUI.DrawRect(new Rect(0,0,200,50), new Color(0.15f, 0.15f, 0.15f));
-                EditorGUILayout.LabelField(capability.GetType().Name);
+                var pos = EditorGUILayout.GetControlRect();
+                int offsetX = 0;
+                pos.y += 2 * j;
+                for (int i = 0; i < datas.Count; i++)
+                {
+                    var data = datas[i];
+                    Color color = stateColor[data.State];
+                    Rect rect = new Rect(offsetX, pos.y, data.Count, 20);
+                    offsetX += data.Count;
+                    GUI.color = color;
+                    EditorGUI.DrawRect(rect, GUI.color);
+                    GUI.color = color;
+                }
+
+                pos.x = FrameSize;
+                EditorGUI.LabelField(pos, exSimilar.Name);
                 EditorGUILayout.EndVertical();
             }
 
