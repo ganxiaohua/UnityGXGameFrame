2a29af92c4c932afe8340bcaea5924fde5d4e8f1
ganxiaohua <543123001@qq.com>
2026-05-27 11:41:50 +0800

    update  sceneBase

--- Runtime/Core/SceneEnitiy/SceneBase.cs
+++ Runtime/Core/SceneEnitiy/SceneBase.cs
@@ -1,14 +1,13 @@
 ﻿using System.Collections.Generic;
 using Cysharp.Threading.Tasks;
 using UnityEngine.SceneManagement;
-using YooAsset;
 
 namespace GameFrame.Runtime
 {
     public class SceneBase : Entity, IInitializeSystem, IScene
     {
         private Dictionary<string, IAssetHandle> sceneHandleDic = new Dictionary<string, IAssetHandle>();
-        
+
         protected virtual string SingleSceneName { get; set; }
 
         public virtual void OnInitialize()
@@ -31,18 +30,17 @@ namespace GameFrame.Runtime
         {
             int versions = Versions;
             var hand = await AssetManager.Instance.LoadSceneAsync(name, sceneMode);
-            if (hand != null && versions != Versions)
+            if (hand == null)
+                return false;
+
+            if (versions != Versions)
             {
                 hand.Release();
                 return false;
             }
-            else if (hand != null)
-            {
-                sceneHandleDic.Add(name, hand);
-                return true;
-            }
 
-            return false;
+            sceneHandleDic.Add(name, hand);
+            return true;
         }
 
         public override void Dispose()
@@ -51,6 +49,7 @@ namespace GameFrame.Runtime
             {
                 sceneHandle.Value.Release();
             }
+
             sceneHandleDic.Clear();
             base.Dispose();
         }
